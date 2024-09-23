2c8c9cc38182aa640640044e3692687d8700c234
Farawa <dzipopo@mail.ru>
2024-09-23 18:06:05 +0300

    add dotween, add animations

--- Assets/Scripts/ClockDisplay.cs
+++ Assets/Scripts/ClockDisplay.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,13 +11,21 @@ public class ClockDisplay : MonoBehaviour
     [SerializeField] private RectTransform minuteArrow;
     [SerializeField] private RectTransform secondArrow;
     [SerializeField] private TextMeshProUGUI timeText;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float clockMoveDuration = 0.5f;
+    [SerializeField] private Ease easeType = Ease.Linear;
 
     public void SetTime(float hours, float minutes, float seconds)
     {
-        hourArrow.rotation = GetRotationFromValue(hours%12, 12);
-        minuteArrow.rotation = GetRotationFromValue(minutes, 60);
-        secondArrow.rotation = GetRotationFromValue(seconds, 60);
+        RotateArrow(hourArrow, GetRotationFromValue(hours % 12, 12));
+        RotateArrow(minuteArrow, GetRotationFromValue(minutes, 60));
+        RotateArrow(secondArrow, GetRotationFromValue(seconds, 60));
         timeText.text = $"{hours}:{minutes}:{seconds}";
+    }
+
+    private void RotateArrow(RectTransform rect, Quaternion angle)
+    {
+        rect.DORotateQuaternion(angle, clockMoveDuration).SetEase(Ease.Linear);
     }
 
     private Quaternion GetRotationFromValue(float value, float maxValue)
